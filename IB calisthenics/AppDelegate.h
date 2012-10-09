e2368ce96849db1ba8d917bb8770eda1114f114b
michael.r.dorsey <michael.r.dorsey@gmail.com>
2012-10-08 22:52:18 -0700

    Added README.md

--- IB calisthenics/AppDelegate.h
+++ IB calisthenics/AppDelegate.h
@@ -12,4 +12,57 @@
 
 @property (assign) IBOutlet NSWindow *window;
 
+// Copy Text Actions
+- (IBAction)showHelloText:(id)sender;
+- (IBAction)showGoodbyeText:(id)sender;
+- (IBAction)copyUserText:(id)sender;
+
+// Show Garish Number Actions
+- (IBAction)showGarishNumberText:(id)sender;
+
+// Season Actions
+- (IBAction)showSeasonText:(id)sender;
+
+// Show Time Now Actions
+- (IBAction)showTimeNow:(id)sender;
+
+// Square Number Actions
+- (IBAction)showSliderValSquared:(id)sender;
+
+// Speech Control Actions
+- (IBAction)changeVoiceType:(id)sender;
+- (IBAction)changeVoiceSpeed:(id)sender;
+- (IBAction)startSpeech:(id)sender;
+- (IBAction)stopSpeech:(id)sender;
+
+// Copy Text Outlets
+
+@property (weak) IBOutlet NSTextField *cpOutputLbl;
+@property (weak) IBOutlet NSTextField *cpInputTxt;
+
+// Show Garish Number Outlets
+
+@property (weak) IBOutlet NSSegmentedControl *garishNumCtrl;
+@property (weak) IBOutlet NSTextField *garishTxtLbl;
+
+// Season Outlets
+@property (weak) IBOutlet NSTextField *seasonOutputLbl;
+@property (weak) IBOutlet NSMatrix *seasonRadioGrp;
+
+// Show Time Outlets
+@property (weak) IBOutlet NSTextField *timeNowLbl;
+
+// Square Number Outlets
+@property (weak) IBOutlet NSSlider *squareSlider;
+@property (weak) IBOutlet NSTextField *sliderValLbl;
+@property (weak) IBOutlet NSTextField *squareValLbl;
+
+// Speech Control Outlets
+@property (weak) IBOutlet NSSegmentedControl *voiceTypeControl;
+@property (weak) IBOutlet NSSlider *voiceSpeedSlider;
+@property (weak) IBOutlet NSButton *voiceSpeakBtn;
+@property (weak) IBOutlet NSButton *voiceShushBtn;
+@property (weak) IBOutlet NSTextField *voiceText;
+@property (weak) IBOutlet NSSpeechSynthesizer *synth;
+
 @end
--- IB calisthenics/AppDelegate.m
+++ IB calisthenics/AppDelegate.m
@@ -12,7 +12,96 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-	// Insert code here to initialize your application
+
 }
 
+// Copy Text Action Impls
+- (IBAction)showHelloText:(id)sender {
+	[[self cpOutputLbl] setStringValue: @"Hello"];
+}
+
+- (IBAction)showGoodbyeText:(id)sender {
+	[[self cpOutputLbl] setStringValue: @"Goodbye"];
+}
+
+- (IBAction)copyUserText:(id)sender {
+	[[self cpOutputLbl] setStringValue:[
+		[self cpInputTxt] stringValue]
+	];
+}
+
+// Show Garish Num Action Impls
+- (IBAction)showGarishNumberText:(id)sender {
+	NSInteger selectedSegment = [sender selectedSegment];
+	NSString *selectedLabel = [sender labelForSegment:[[self garishNumCtrl] selectedSegment]];
+	
+	NSString *lblString = [NSString stringWithFormat:@"%ld%@%@",
+						   selectedSegment, @": ", selectedLabel];
+	[[self garishTxtLbl] setStringValue: lblString];
+}
+
+// Season Action Impls
+- (IBAction)showSeasonText:(id)sender {
+	NSInteger selectedSeason = [sender selectedRow];
+	NSString *seasonLabel;
+	
+	switch(selectedSeason) {
+		case 0: seasonLabel = @"December";
+			break;
+		case 1: seasonLabel = @"March";
+			break;
+		case 2: seasonLabel = @"June";
+			break;
+		case 3: seasonLabel = @"September";
+			break;
+	}
+	
+	[[self seasonOutputLbl] setStringValue: seasonLabel];
+}
+
+// Current Time Action Impls
+- (IBAction)showTimeNow:(id)sender {
+	NSDate *now = [NSDate date];
+	NSDateFormatter *outputFormatter = [[NSDateFormatter alloc] init];
+	[outputFormatter setDateFormat:@"MM/dd/yy hh:mm:ss a"];
+	[[self timeNowLbl] setStringValue:[outputFormatter stringFromDate: now]];
+}
+
+// Square Slider Action Impls
+- (IBAction)showSliderValSquared:(id)sender {
+	double sliderVal = [[self squareSlider] floatValue];
+	double sliderSquared = pow(sliderVal, 2);
+	NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+	[formatter setNumberStyle: NSNumberFormatterSpellOutStyle];
+	
+	[[self sliderValLbl] setDoubleValue: sliderVal];
+	[[self squareValLbl] setStringValue: [formatter stringFromNumber: [NSNumber numberWithDouble: sliderSquared]]];
+}
+
+// Speech Action Impls
+- (IBAction)changeVoiceType:(id)sender {
+	bool wasSpeaking = false;
+	if([[self synth] isSpeaking]) {
+		wasSpeaking = true;
+	}
+	
+	[[self synth] setVoice: [@"com.apple.speech.synthesis.voice." stringByAppendingString: [sender labelForSegment:[sender selectedSegment]]]];
+	[[self voiceSpeedSlider] setFloatValue:[[self synth] rate]];
+	
+	if(wasSpeaking) {
+		[[self synth] startSpeakingString: [[self voiceText] stringValue]];
+	}
+}
+
+- (IBAction)changeVoiceSpeed:(id)sender {
+	[[self synth] setRate: [sender floatValue]];
+}
+
+- (IBAction)startSpeech:(id)sender {
+	[[self synth] startSpeakingString: [[self voiceText] stringValue]];
+}
+
+- (IBAction)stopSpeech:(id)sender {
+	[[self synth] stopSpeaking];
+}
 @end
